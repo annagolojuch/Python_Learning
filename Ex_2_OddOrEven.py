4ea1f22861c912932e4aa6bb5607817d984d01d0
annagol <agolojuch@gmail.com>
2023-02-05 20:50:42 +0100

    2 Ex *

--- Ex_2_OddOrEven.py
+++ Ex_2_OddOrEven.py
@@ -1,6 +1,14 @@
 num = int(input("Podaj liczbę >> "))
+check = int(input("Podaj drugą liczbę >>"))
 
-if num % 2 == 0:
-    print("Została podana liczba PARZYSTA")
+# if num % 4 == 0:
+#     print("To liczba podzielna przez 4")
+# elif num % 2 == 0:
+#     print("Została podana liczba PARZYSTA")
+# else:
+#     print("Została podana liczba NIEPARZYSTA")
+
+if num == check:
+    print("Liczby sa takie same")
 else:
-    print("Została podana liczba NIEPARZYSTA")
+    print("Podano rózne liczby!")
