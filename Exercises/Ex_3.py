65340f0a87db3648f917d4f2d86b0e2af8115632
annagol <agolojuch@gmail.com>
2023-02-05 21:33:57 +0100

    Ex 3 * done

--- Exercises/Ex_3.py
+++ Exercises/Ex_3.py
@@ -1,5 +1,23 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
-for x in a:
-    if x <5:
-        print(x)
+#pierwsza część
+x = []
+for i in a:
+    if i < 5:
+        x.append(i)
+        x.sort()
+print(x)
+print()
+
+#druga część
+y = []
+num = int(input("Wprowadź liczbę >>"))
+for j in a:
+    if j < num:
+        y.append(j)
+        y.sort()
+print(y)
+
+
+
+
