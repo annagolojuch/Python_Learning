40c16ea353c3ae0440501912b315068c09d1360e
annagol <agolojuch@gmail.com>
2023-03-03 14:39:18 +0100

    nauka

---
+++ Learning/Functions/wbudowane.py
@@ -0,0 +1,22 @@
+characters = ['k', 'b', 'c', 'j', 'z', 'w']
+print('Pierwsza:', min(characters))
+print('Ostatnia:', max(characters))
+
+# print(30*'-')
+#
+# var1 = 'Python'
+# var2 = ('Python')
+# var3 = ('Python',)
+# var4 = ['Python']
+# var5 = {'Python'}
+# print(isinstance(var1, tuple))
+# print(isinstance(var2, tuple))
+# print(isinstance(var3, tuple))
+# print(isinstance(var4, tuple))
+# print(isinstance(var5, tuple))
+
+# print(30*'-')
+#
+# x = -1.5
+# expression = 'x**2 + x'
+# print(eval(expression))
